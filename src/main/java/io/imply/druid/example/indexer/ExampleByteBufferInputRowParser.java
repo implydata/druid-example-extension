e6f8180747e663efcc292a5f53dee326737d4a7c
Clint Wylie <cjwylie@gmail.com>
2019-02-14 13:28:05 -0800

    add missing licenses

--- src/main/java/io/imply/druid/example/indexer/ExampleByteBufferInputRowParser.java
+++ src/main/java/io/imply/druid/example/indexer/ExampleByteBufferInputRowParser.java
@@ -1,16 +1,30 @@
+/*
+ * Copyright 2019 Imply Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.imply.druid.example.indexer;
 
-import com.google.common.annotations.VisibleForTesting;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableList;
 import org.apache.druid.data.input.ByteBufferInputRowParser;
 import org.apache.druid.data.input.InputRow;
-import org.apache.druid.data.input.impl.MapInputRowParser;
 import org.apache.druid.data.input.impl.ParseSpec;
 import org.apache.druid.data.input.impl.StringInputRowParser;
 import org.apache.druid.java.util.common.StringUtils;
-import org.apache.druid.java.util.common.parsers.Parser;
 
 import java.nio.ByteBuffer;
 import java.util.Base64;
